b7b05985e1c309f513822cf7bcc442ff84b3be4e
Kdaw <karldaw@hotmail.com>
2018-02-21 11:10:59 +0000

    added Post scaffolding

---
+++ app/src/main/java/com/example/karld/marshmallowenp/Post.java
@@ -0,0 +1,28 @@
+package com.example.karld.marshmallowenp;
+
+/**
+ * Created by karld on 21/02/2018.
+ */
+
+public class Post {
+    private static String postID;
+    private static String userID;
+    private static String details;
+    private static String location; //TODO update with location details for Google Maps
+    //TODO add image type variable
+    private static int reviewed; // either 1 or 0 to add to firebase. Use YES or NO
+    private static final int YES = 1;
+    private static final int NO = 0;
+
+    private Post(String uID, String deets, String loc){
+        //push for post id then add rest as child
+        userID = uID;
+        details = deets;
+        location = loc;
+        reviewed = NO;
+    }
+
+    public static Post CreatePost(String id, String dts, String loca) {
+        return new Post(id, dts, loca);
+    }
+}
