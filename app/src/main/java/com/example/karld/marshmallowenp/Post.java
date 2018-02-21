9b0a18b8d924f72e1e75483530a42a6251079e7d
Kdaw <karldaw@hotmail.com>
2018-02-21 11:25:18 +0000

    Review class scaffolded

--- app/src/main/java/com/example/karld/marshmallowenp/Post.java
+++ app/src/main/java/com/example/karld/marshmallowenp/Post.java
@@ -13,6 +13,9 @@ public class Post {
     private static int reviewed; // either 1 or 0 to add to firebase. Use YES or NO
     private static final int YES = 1;
     private static final int NO = 0;
+    private static int available;
+    //TODO add list of bidders
+    private static String bidWonBy;
 
     private Post(String uID, String deets, String loc){
         //push for post id then add rest as child
