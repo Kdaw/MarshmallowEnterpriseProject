e9b653b87546c1626cfb77e756c7e5392f7d68c1
Jacob NORRIS <s6105214@linux12.scm.tees.ac.uk>
2018-02-21 13:47:57 +0000

    edited createpostactivity alot, edit text fields now saved  with the saved input being written to database

--- app/src/main/java/com/example/karld/marshmallowenp/CreatePostActivity.java
+++ app/src/main/java/com/example/karld/marshmallowenp/CreatePostActivity.java
@@ -4,10 +4,14 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class CreatePostActivity extends AppCompatActivity implements View.OnClickListener{
+public class CreatePostActivity extends AppCompatActivity /*implements View.OnClickListener*/{
 
     //todo Get CreatePostActivity to write to the database
     //todo Get correct data from gareth of what needs to be in post
@@ -19,58 +23,6 @@ public class CreatePostActivity extends AppCompatActivity implements View.OnClic
     //https://github.com/firebase/quickstart-android/blob/master/database/app/src/main/java/
     // com/google/firebase/quickstart/database/NewPostActivity.java#L41-L41
 
-
-    public static class Post
-    {
-        public String postTitle;
-        public String postDescription;
-        public String postOwner; //foreign key (kinda)
-
-
-        //Default Constructor
-        public Post()
-        {
-
-        }
-
-        //Constructor with parameters
-        public Post(String title, String description, String owner)
-        {
-            title = postTitle;
-            description = postDescription;
-            owner = postOwner;
-        }
-
-        public String getPostTitle()
-        {
-            return postTitle;
-        }
-
-        public void setPostTitle(String title)
-        {
-            title = postTitle;
-        }
-
-        public String getPostDescription()
-        {
-            return postDescription;
-        }
-
-        public void setPostDescription(String description)
-        {
-            description = postDescription;
-        }
-
-        public String getPostOwner()
-        {
-            return postOwner;
-        }
-
-        public void setPostOwner(String owner)
-        {
-            owner = postOwner;
-        }
-    }
 
 
     /*
@@ -90,40 +42,94 @@ public class CreatePostActivity extends AppCompatActivity implements View.OnClic
     }
 
 
+    private DatabaseReference mRef;
+
+    String title;
+    String details;
+    String location;
+
+
+    EditText titleInput;
+    EditText detailInput;
+    EditText locationInput;
+
+    Button postButton;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_post);
+//        postButton.setOnClickListener(CreatePostActivity.this);
+        //new View.OnClickListener()
+
+//        //todo - figure out symbol issue - FIXED
+
+        titleInput = (EditText) findViewById(R.id.editText_Title);
+        detailInput = (EditText) findViewById(R.id.editText_Details);
+        locationInput = (EditText) findViewById(R.id.editText_Location);
 
 
-        Button postButton;
-        //todo - figure out symbol issue - FIXED
         postButton = (Button) findViewById(R.id.CreatePostButton);
-        postButton.setOnClickListener(CreatePostActivity.this);
+        postButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                title = titleInput.getText().toString();
+                details = detailInput.getText().toString();
+                location = locationInput.getText().toString();
+
+                showToast(title);
+                showToast(details);
+                showToast(location);
+
+                Post newPost = Post.CreatePost("0001", title, details, location);
+
+
+
+                mRef =  FirebaseDatabase.getInstance().getReference();
+                //todo get this working for the random id to create post values under
+               // String postid = mRef.push().toString();
+                String postid = "ikxsfbgksjdgb";
+                mRef.child("Posts").setValue(postid);
+                mRef.child("Posts").child(postid).child("title").setValue(title);
+                mRef.child("Posts").child(postid).child("details").setValue(details);
+                mRef.child("Posts").child(postid).child("location").setValue(location);
+
+
+
+
+
+                //mRef.setValue(Post.CreatePost("0001", title, details, location));
+            }
+        });
+
+
+    }
+
+    private void showToast(String text)
+    {
+        Toast.makeText(CreatePostActivity.this, text, Toast.LENGTH_SHORT).show();
     }
 
 
-    @Override
-    public void onClick(View v)
-    {
 
-        FirebaseDatabase database =  FirebaseDatabase.getInstance();
-        Post newPost = new Post("TestPost1","TestDescription1","TestUser1");
-
-        //todo - add validation to prevent duplication of users in database - potentially not the way i'll be doing it
-
+//
+//        FirebaseDatabase database =  FirebaseDatabase.getInstance();
+//        Post newPost = new Post("TestPost1","TestDescription1","TestUser1");
+//
+//        //todo - add validation to prevent duplication of users in database - potentially not the way i'll be doing it
+//
 //        DatabaseReference mRef =  database.getReference().child("Posts");
 //        mRef = mRef.push();
 //        mRef.setValue("test");
-
-        DatabaseReference mRef =  database.getReference().child("testUser1").child("post title");
-        mRef.setValue("test post title");
-
-        mRef =  database.getReference().child("testUser1").child("post description");
-        mRef.setValue("test description");
-
-    }
+//
+//        DatabaseReference mRef =  database.getReference().child("Posts").child("post title");
+//        mRef.setValue("test post title");
+//
+//        mRef =  database.getReference().child("testUser1").child("post description");
+//        mRef.setValue("test description");
 }
 
 //.child("TestUser1")
--- app/src/main/java/com/example/karld/marshmallowenp/Post.java
+++ app/src/main/java/com/example/karld/marshmallowenp/Post.java
@@ -7,6 +7,7 @@ package com.example.karld.marshmallowenp;
 public class Post {
     private static String postID;
     private static String userID;
+    private static String postTitle;
     private static String details;
     private static String location; //TODO update with location details for Google Maps
     //TODO add image type variable
@@ -15,17 +16,19 @@ public class Post {
     private static final int NO = 0;
     private static int available;
     //TODO add list of bidders
+        private static String bidders;
     private static String bidWonBy;
 
-    private Post(String uID, String deets, String loc){
+    private Post(String uID, String title, String deets, String loc){
         //push for post id then add rest as child
         userID = uID;
+        postTitle = title;
         details = deets;
         location = loc;
         reviewed = NO;
     }
 
-    public static Post CreatePost(String id, String dts, String loca) {
-        return new Post(id, dts, loca);
+    public static Post CreatePost(String id, String ttl, String dts, String loca) {
+        return new Post(id, ttl, dts, loca);
     }
 }
