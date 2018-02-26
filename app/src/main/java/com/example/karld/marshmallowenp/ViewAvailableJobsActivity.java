9768061641b09e443781a450c627794cbd3714fd
Kdaw <karldaw@hotmail.com>
2018-02-26 11:36:34 +0000

    Basic setup for listview done. Working on loading items from firebase

---
+++ app/src/main/java/com/example/karld/marshmallowenp/ViewAvailableJobsActivity.java
@@ -0,0 +1,80 @@
+package com.example.karld.marshmallowenp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+
+public class ViewAvailableJobsActivity extends AppCompatActivity {
+
+    /**
+     * Implementation of the following was done using the information found at
+     * http://www.techotopia.com/index.php/A_Firebase_Realtime_Database_List_Data_Tutorial
+     * This describes how to fill a list view from a Firebase Realtime Database
+     */
+
+    private ArrayList<String> listItems = new ArrayList<>();
+    private ArrayList<String> listKeys = new ArrayList<>();
+    private ArrayAdapter<String> adapter;
+    private ListView availableJobs;
+    private FirebaseDatabase fbDatabase = FirebaseDatabase.getInstance();
+    private DatabaseReference dbRef = fbDatabase.getReference("Posts");
+    private int itemSelected;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_available_jobs);
+
+        availableJobs = (ListView) findViewById(R.id.ListView_AvailableJobs);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_single_choice, listItems);
+        availableJobs.setAdapter(adapter);
+        availableJobs.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+        availableJobs.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                itemSelected = position;
+            }
+        });
+        addChildEventListener();
+    }
+
+    private void addChildEventListener() {
+        ChildEventListener cListener = new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        }
+    }
+}
