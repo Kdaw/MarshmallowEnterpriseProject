abe45601543248e35d226b2b90135654e50633ba
Donnzzeti <s6205345@live.tees.ac.uk>
2018-02-28 21:05:11 +0000

    Working Version Of Nav Menu 1.0 -- Added Free Icons - Placement holders in place, N0 links to buttons!

--- app/src/main/java/com/example/karld/marshmallowenp/MainActivity.java
+++ app/src/main/java/com/example/karld/marshmallowenp/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.karld.marshmallowenp;
 
 import android.content.Intent;
+import android.drm.DrmStore;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -14,20 +18,43 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
 
     private FirebaseAuth mAuth;
     private DatabaseReference mDatabase;
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle   mToggle;
     @Override
     //added activity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mAuth = FirebaseAuth.getInstance();
 
+        // Slider Menu Code ----------------------------------------------------------------------------------------------
+        mDrawerLayout = (DrawerLayout) findViewById (R.id.drawerLayout);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        // --------------------------------------------------------------------------------------------------------------
+
+        // Fire BaseCode -----------------------------------------------------------------------------------------------
+        mAuth = FirebaseAuth.getInstance();
         FirebaseUser currentUser = mAuth.getCurrentUser();
         mDatabase = FirebaseDatabase.getInstance().getReference().child("Users")/*.child(currentUser.getUid())*/;
         //mDatabase.setValue(currentUser.getUid());
         mDatabase.child(currentUser.getUid()).child("Rate").setValue(0);
         mDatabase.child(currentUser.getUid()).child("Email").setValue(currentUser.getEmail());
+
     }
 
+
+    // Enables Nav menu click -  Allows for both slide and on click access.
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        if(mToggle.onOptionsItemSelected(item))
+    {
+        return  true;
+    }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     public void onClick(View v){
