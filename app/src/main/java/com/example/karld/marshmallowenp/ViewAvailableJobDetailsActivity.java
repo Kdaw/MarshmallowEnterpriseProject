e8094932b3f80f778dc087d31bd3939752df36d0
Donnzzeti <s6205345@live.tees.ac.uk>
2018-03-04 20:16:14 +0000

    Splash Implemented -- Nice Splash Screen add! && With added animation!
    Animation folder added! -- For Splash screen!
    
    Nav Menu Activities merger -- More Activities been merged with Nav Menu! Only 2 remain togo -- Bug / Layout related Issues!
    
    Files Added -- More images and files added for splash screen. Also gradient added.
    
    Main screen background added! --  Given the Main screen a background of gradient file along with the nav header!
    
    AndroidManifest changes! -- changed load order of app, app will now Load the Splash screen first then go into log in menu! Better on performance also!
    
    Performance increases! -- due to splash screen.
    
    Loads of changes!!

--- app/src/main/java/com/example/karld/marshmallowenp/ViewAvailableJobDetailsActivity.java
+++ app/src/main/java/com/example/karld/marshmallowenp/ViewAvailableJobDetailsActivity.java
@@ -1,8 +1,12 @@
 package com.example.karld.marshmallowenp;
 
 import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -13,6 +17,8 @@ import com.google.firebase.database.ValueEventListener;
 
 public class ViewAvailableJobDetailsActivity extends AppCompatActivity {
 
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
     private FirebaseDatabase fbDatabase = FirebaseDatabase.getInstance();
     private DatabaseReference dbRef = fbDatabase.getReference("Posts");
 
@@ -21,6 +27,35 @@ public class ViewAvailableJobDetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_available_job_details);
+
+        // Slider Menu Code ----------------------------------------------------------------------------------------------
+        mDrawerLayout = (DrawerLayout) findViewById (R.id.drawerLayout);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // Nav Menu linking - Links Activities From Nav Menu ---------------------------------------------------------------
+        NavigationView nV =(NavigationView)findViewById(R.id.nav_menu);
+        nV.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                // Handle navigation view item clicks.
+                int id = menuItem.getItemId();
+
+                if (id == R.id.nav_home) {
+                    Intent in = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(in);
+                } else if (id == R.id.nav_account) {
+                    Intent in = new Intent(getApplicationContext(), ProfileSettingsActivity.class);
+                    startActivity(in);
+                } else if (id == R.id.nav_TOS) {
+                    Intent in = new Intent(getApplicationContext(), TermsOfService.class);
+                    startActivity(in);
+                }
+                return true;
+            }
+        });
 
         final TextView title = findViewById(R.id.textView_JobDetailTitle);
         final TextView detail = findViewById(R.id.textView_JobDetailsDescription);
@@ -58,5 +93,16 @@ public class ViewAvailableJobDetailsActivity extends AppCompatActivity {
             public void onCancelled(DatabaseError databaseError) {
             }
         });
+    }
+
+    // Enables Nav menu click -  Allows for both slide and on click access.
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        if(mToggle.onOptionsItemSelected(item))
+        {
+            return  true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
